e5c430121aef31afad3b2b03720a00cba9b6e446
Louis-Philippe Gauthier <lpgauth@gmail.com>
2023-11-02 22:10:26 -0400

    Tweak metrics

--- src/whitecap_connection.erl
+++ src/whitecap_connection.erl
@@ -12,7 +12,8 @@
 
 -record(state, {
     bin_patterns,
-    socket
+    socket,
+    timestamp
 }).
 
 %% public
@@ -22,13 +23,26 @@ start_link(Socket, Opts) ->
 recv_loop(Socket, Opts) ->
     recv_loop(<<>>, undefined, #state {
         bin_patterns = whitecap_protocol:bin_patterns(),
-        socket = Socket
+        socket = Socket,
+        timestamp = os:system_time()
     }, 0, Opts).
 
 %% private
+close(Socket, Requests, Timestamp) ->
+    gen_tcp:close(Socket),
+    telemetry:execute([whitecap, connections, close], #{}),
+    telemetry:execute([whitecap, connections, stats], #{
+        duration => duration(Timestamp),
+        keep_alive => Requests + 1
+    }).
+
+duration(Timestamp) ->
+    (os:system_time() - Timestamp) div 1000000000.
+
 parse_requests(Data, Req, #state {
         bin_patterns = BinPatterns,
-        socket = Socket
+        socket = Socket,
+        timestamp = Timestamp
     } = State, N, Opts) ->
 
     case whitecap_protocol:request(Data, Req, BinPatterns) of
@@ -40,16 +54,13 @@ parse_requests(Data, Req, #state {
                     Headers2 = overwrite_key(Headers, "Connection", "close", false),
                     Response = whitecap_handler:response(Status, Headers2, Body),
                     gen_tcp:send(Socket, Response),
-                    gen_tcp:close(Socket),
-                    telemetry:execute([whitecap, connections, close], #{}),
+                    close(Socket, N, Timestamp),
                     telemetry:execute([whitecap, connections, max_keepalive], #{}),
-                    telemetry:execute([whitecap, connections, requests], #{keep_alive => N + 1}),
                     ok;
                 false ->
                     Response = whitecap_handler:response(Status, Headers, Body),
                     gen_tcp:send(Socket, Response),
                     parse_requests(Rest, undefined, State, N + 1, Opts)
-
             end;
         {ok, #whitecap_req {} = Req2, Rest} ->
             recv_loop(Rest, Req2, State, N, Opts);
@@ -57,9 +68,7 @@ parse_requests(Data, Req, #state {
             recv_loop(Data, Req, State, N, Opts);
         {error, _Reason} ->
             gen_tcp:send(Socket, whitecap_handler:response(501, [{"Connection", "close"}])),
-            gen_tcp:close(Socket),
-            telemetry:execute([whitecap, connections, close], #{}),
-            telemetry:execute([whitecap, connections, requests], #{keep_alive => N}),
+            close(Socket, N, Timestamp),
             ok
     end.
 
@@ -72,7 +81,7 @@ overwrite_key([{Key, _Value} | T], Key, Value2, _Overridden) ->
 overwrite_key([{Key, Value} | T], Key2, Value2, Overridden) ->
     [{Key, Value} | overwrite_key(T, Key2, Value2, Overridden)].
 
-recv_loop(Buffer, Req, #state {socket = Socket} = State, N, Opts) ->
+recv_loop(Buffer, Req, #state {socket = Socket, timestamp = Timestamp} = State, N, Opts) ->
     {ok, ReceiveTimeout} = whitecap_config:get(receive_timeout),
     case gen_tcp:recv(Socket, 0, ReceiveTimeout) of
         {ok, Data} ->
@@ -80,14 +89,10 @@ recv_loop(Buffer, Req, #state {socket = Socket} = State, N, Opts) ->
             parse_requests(Data2, Req, State, N, Opts);
         {error, timeout} ->
             gen_tcp:send(Socket, whitecap_handler:response(408, [{"Connection", "close"}])),
-            gen_tcp:close(Socket),
-            telemetry:execute([whitecap, connections, close], #{}),
-            telemetry:execute([whitecap, connections, requests], #{keep_alive => N}),
+            close(Socket, N, Timestamp),
             telemetry:execute([whitecap, connections, timeout], #{}),
             ok;
         {error, closed} ->
-            gen_tcp:close(Socket),
-            telemetry:execute([whitecap, connections, close], #{}),
-            telemetry:execute([whitecap, connections, requests], #{keep_alive => N}),
+            close(Socket, N, Timestamp),
             ok
     end.
